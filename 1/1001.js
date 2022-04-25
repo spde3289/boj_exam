a0d1d7664f48260bec9d20cdff095660a2848d36
spde3289 <spde3289@naver.com>
2022-04-25 22:17:41 +0900

    ss

--- 1/1001.js
+++ 1/1001.js
@@ -1,4 +1,10 @@
-var a = 1;
-var b = 2;
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '1/1001.txt';
+
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+var a = +input[0];
+var b = +input[1];
 console.log(a-b);
- 
+
+
