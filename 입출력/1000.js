2e1761ec3410bc581cb61ffb8320e63c99d0a94a
spde3289 <spde3289@naver.com>
2022-04-25 10:14:43 +0900

    ss

--- 입출력/1000.js
+++ 입출력/1000.js
@@ -1,8 +1,5 @@
-<script>
-    var input = confirm('확인하시겠습니까?');
-    if (input === true){
-        alert('확인해주셔서 감사합니다.')
-    } else {
-        alert('안타깝네요~')
-    }
-</script>
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
