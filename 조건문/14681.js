245548c07204a3ec22eb8e64aa1d677649322e35
spde3289 <spde3289@naver.com>
2022-04-26 21:29:03 +0900

    ss

---
+++ 조건문/14681.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const x = +input[0];
+const y = +input[1];
+
+if ( x>0 && y>0 ){
+    console.log(1);
+} else if ( x<0 && y>0){
+    console.log(2);
+} else if ( x>0 && y<0){
+    console.log(3);
+} else{
+    console.log(4);
+}
