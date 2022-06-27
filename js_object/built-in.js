ea8db7cf387123a49048c30ed78e1f170338899c
spde3289 <spde3289@naver.com>
2022-06-28 01:26:56 +0900

    ss

--- js_object/built-in.js
+++ js_object/built-in.js
@@ -1,1 +1,12 @@
 console.log(Math.PI,2)
+
+const MyMath = {
+    PI:Math.PI,
+    random:function(){
+        return Math.random();
+    },
+    floor:function(val){
+        return Math.floor(val);
+    }
+}
+console.log(MyMath.floor(3.5,6.6))
