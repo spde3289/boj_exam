fd6aaf2d846b651df6ee68a2a81936796f9195f8
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-06-29 05:39:14 +0900

    ss

--- 조건문/14681.js
+++ 조건문/14681.js
@@ -1,18 +1,42 @@
 const readline = require("readline");
 const rl = readline.createInterface({
-  input:  process.stdin,
+  input: process.stdin,
   output: process.stdout
 });
 
-let inputNumber
+let input = [];
 
-rl.on('line', function (line) {
-    inputNumber = line
-    rl.close()
-}).on('close', function () {
-    if(line < 10){
-        console.log('작은수입니다.')
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  ///////////////////////////////
+  const x = input[0];
+  const y = input[1];
+
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  }
+  /////////////////////////////
+  process.exit();
+});
+
+/*  const input = {
+    x : 12,
+    y : 5
+ };
+
+ if (input.x > 0 && input.y > 0){
+    console.log('1');
+ }else if (input.x < 0 && input.y > 0){
+    console.log('2');
+ }else if (input.x < 0 && input.y < 0){
+     console.log('3');
     }else{
-        console.log('큰수입니다.')
-    }
-})
+     console.log('4')
+ } */
--- 조건문/2884.js
+++ 조건문/2884.js
@@ -1,1 +1,30 @@
-c
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+
+rl.on("line", function (line) {
+  input = line.split(' ').map( function(el){return parseInt(el)});
+}).on("close", function () {
+  ///////////////////////////////
+    let num1 = input[0];
+    let num2 = input[1];
+
+    if(num2 - 45 < 0){
+        num2 = 60 + (num2 - 45);
+        num1 -= 1;
+        if(num1 === -1){
+            num1 = 23;
+        }
+    }else{
+        num2 -= 45;
+    };
+    console.log(num1 + ' ' + num2)
+  
+  /////////////////////////////
+  process.exit();
+});
