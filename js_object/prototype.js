8ea6eea9e70d0faf97084aa18923eea596124ec5
spde3289 <spde3289@naver.com>
2022-06-28 16:40:01 +0900

    Create prototype.js

---
+++ js_object/prototype.js
@@ -0,0 +1,30 @@
+function Person(name, first, secound, third) {
+    this.name=name;
+    this.first=first;
+    this.secound=secound;
+    this.third=third;
+};
+
+const kim = new Person('kim', 10 ,20, 30)
+const lee = new Person('lee', 20, 10, 10)
+
+Person.prototype.sum = function() {
+    return 'prototype : '+(this.first + this.secound);
+}
+lee.sum = function() {
+    return 'lee : '+(this.first + this.secound + this.third);
+}
+
+console.log("kim.sum()",kim.sum())
+console.log("lee.sum()",lee.sum())
+
+
+//const d1 = new Date(2011-11-11)
+//console.log(d1.getFullYear())
+//console.log(d1.getMonth()+1)
+//console.log(d1.getDate())
+//console.log(Date)
+
+
+//console.log("person", Person())
+//console.log("person",new Person("lee",4,0,6).sum()) 
