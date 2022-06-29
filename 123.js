b09dc59ae29ae1f16cfcdc23196f2e4ed36c88e5
spde3289 <spde3289@naver.com>
2022-06-29 20:45:48 +0900

    ss

--- 123.js
+++ 123.js
@@ -1,24 +1,35 @@
-const arr = [1, 2, 1, 6];
+const arr = [1, 3, 2,4];
+let price
+for (let i = 0; i < arr.length; i++) {
+    const dice1 = arr[i];
 
-let dupYn = false;
-for(let i = 0; i < arr.length; i++) {
-  const currElem = arr[i];
-  
-  for(let j = i+1; j < arr.length; j++) {
-    if(currElem === arr[j]) {
-      dupYn = true;
-      console.log(arr[i],arr[j])
-      if(dupYn = true){
-      arr.sort()
-      }
-      break;
+    for (let j = i + 1; j < arr.length; j++) {
+        const dice2 = arr[j];
+        /* if(currElem === arr[j]) {
+          dupYn = true;
+          console.log(arr[i],arr[j])
+          if(dupYn = true){
+          arr.sort()
+          } */
+
+        for (let k = i + 2; k < arr.length; k) {
+            const dice3 = arr[k];
+            if (dice3 === dice1 && dice1 === dice2) {
+                price = 1000 + (arr[k] * 1000);
+                console.log(arr,"1")
+            } else if (dice3 === dice2 || dice3 === dice1 || dice2 === dice1){
+                price = 1000 + (arr[k] * 100)
+                console.log(arr,"2")
+                console.log(arr[i],arr[j],arr[k])
+            } else {
+                price = Math.max(...arr)*100
+                console.log(arr,"3")
+            }
+            console.log(price)
+            break;
+
+        }
+
     }
-  }
-  
-  if(dupYn)  {
-    break;
-  }
 }
 
-console.log("DupYn : " + dupYn);
-console.log(arr);
