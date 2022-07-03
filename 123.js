fb948068beeddece0e76925d686284607b72d7ee
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-07-04 03:24:23 +0900

    ss

--- 123.js
+++ 123.js
@@ -1,17 +1,13 @@
-let input = `5\n
-1 1\n
-2 2\n 
-3 3\n 
-4 4\n 
-5 5\n
-`
+let input = 6;
+let answer = '';
+
+function a(){
+    for (var i = 1; i <= input; i++){
+        answer += i +"\n";
+    }
+};
+a()
 
 
-let answer;
-    for(let i=1; i <= input[0]; i++){
-        let a = +input[i][0];
-        let b = +input[i][1];
-        answer += `${a} + ${b} \n`;
-    };
-    console.log(answer);
-    console.log(input[3].split(' ')[1]);
+
+console.log(answer);
--- 반복문/2741.js
+++ 반복문/2741.js
@@ -3,4 +3,15 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-let 
+const input = [];
+let answer = "";
+rl.on('line', line =>{
+    input.push(line);
+}).on('close', () =>{
+    let Input = Number(input);
+    for(let i = 1; i <= Input; i++){
+        answer += i + "\n";
+    };
+    console.log(answer);
+    process.exit();
+});
