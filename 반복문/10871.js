d534139391e1082c1a11d0139d126dbaed8bed25
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-07-11 00:05:43 +0900

    ss

--- 반복문/10871.js
+++ 반복문/10871.js
@@ -4,15 +4,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 const input = [];
+let answer = '';
 rl.on('line', line => {
     input.push(line);
 }).on('close', () => {
-    let N = input[0].split(' ')[0];
-    let X = input[0].split(' ')[1];
-    let A = input[1].map();
-    let answer = '';
+    let N = Number(input[0].split(' ')[0]);
+    let X = Number(input[0].split(' ')[1]);
+    let A = input[1].split(' ');
     for(let i = 0; i < N; i++){
-        if ( A[i] < X) {
+        if ( Number(A[i]) < X) {
             answer += A[i] +' ';
         };
     };
