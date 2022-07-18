3e6436c411ebb132d7dfbe613347fc1ae5167a11
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-07-19 06:33:10 +0900

    ss

--- 123.js
+++ 123.js
@@ -1,26 +1,2 @@
-const line = ['5',
-    'OOXXOXXOOO',
-    'OOXXOOXXOO',
-    'OXOXOXOXOXOXOX',
-    'OOOOOOOOOO',
-    'OOOOXOOOOXOOOOX'];
-const input = line.map( el => el.split(''));
-
-const N = input[0];
-for (let i = 1; i <= N; i++){
-    let answer = 0;
-    let k = 1;
-    
-    for(let j = 0;j < input[i].length; j++){
-        if(input[i][j] === 'O'){
-            answer += Number(k);
-            k++;
-        }else{
-            k = 1;
-        };
-    };
-    console.log(answer);
-    
-};
-
-
+let a = 23
+console.log(a.toFixed(3),'%\n')
---
+++ 1차원 배열/4344.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const input = [];
+rl.on('line', line => {
+    input.push(line);
+}).on('close', () => {
+    const C = input[0];
+    for(let i =1; i < C; i++){
+        const Score = input[i].split(' ').map(el => Number(el));
+        const N = input[i][0];
+        let average = 0;
+        let sum = 0;
+        for(let j = 1;j < N; j++){
+            sum += Score[j];
+        };
+        average = sum/N;
+        console.log(average.toFixed(3),'%\n');
+    };
+    process.exit();
+});
