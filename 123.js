54915ba7a0de33aaad584b22ac079417129a9cd1
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-07-20 05:01:35 +0900

    ss

--- 123.js
+++ 123.js
@@ -1,2 +1,31 @@
-let a = 23
-console.log(a.toFixed(3),'%\n')
+const input = [
+    '5',
+    '5 50 50 70 80 100',
+    '7 100 95 90 80 70 60 50',
+    '3 70 90 80',
+    '3 70 90 81',
+    '9 100 99 98 97 96 95 94 93 91'
+];
+
+const C = input[0];
+console.log(C)
+for (let i = 1; i <= C; i++) {
+    const Score = input[i].split(' ').map(el => Number(el));
+    const N = input[i][0];
+    let average = 0;
+    let sum = 0;
+    for (let j = 1; j <= N; j++) {
+        sum += Score[j];
+    };
+    average = sum / N;
+    
+    console.log(average, '%');
+}; 
+/* let N = 5
+for (let i = 1; i <= N; i++){
+    
+
+    console.log(i);
+}; */
+let aaa = (50+50+70+80+100 )/5
+console.log(aaa)
--- 1차원 배열/4344.js
+++ 1차원 배열/4344.js
@@ -7,8 +7,8 @@ const input = [];
 rl.on('line', line => {
     input.push(line);
 }).on('close', () => {
-    const C = input[0];
-    for(let i =1; i < C; i++){
+    const C = input[0].split(' ');
+    for(let i =1; i <= C; i++){
         const Score = input[i].split(' ').map(el => Number(el));
         const N = input[i][0];
         let average = 0;
@@ -16,8 +16,8 @@ rl.on('line', line => {
         for(let j = 1;j < N; j++){
             sum += Score[j];
         };
-        average = sum/N;
-        console.log(average.toFixed(3),'%\n');
+        average = (sum/N).toFixed(3);
+        console.log(average,'%\n');
     };
     process.exit();
 });
