3f4dcf854c89a75b37c32c4ecfcb4a72bad8483f
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-12-08 22:24:03 +0900

    Update 10807.js

--- 1차원 배열/10807.js
+++ 1차원 배열/10807.js
@@ -3,14 +3,21 @@ const rl = readline.createInterface({
     input: process.stdin,
     output : process.stdout
 });
+const input = [];
+const answer = [];
 rl.on('line', line => {
     input.push(line);
 }).on('close', () => {
-
-    let integer = input[1].split(' ');
+    const N = Number(input[0]);
+    let integer = input[1].split(' ').map(el=>Number(el));
     const V = Number(input[2]);
     
-    const answer = integer.filter(el => el==V);
+    for(let i = 0; i <= N; i++){
+        if(integer[i] === V){
+            answer.push(integer[i]);
+        };
+    };
+
     console.log(answer.length);
 
     process.exit();
