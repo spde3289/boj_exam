fb1cf7207de06b7403319fbce7f2a788e576cc1b
spde3289 <spde3289@naver.com>
2024-02-18 20:15:03 +0900

    새싹

---
+++ 심화1/25083.js
@@ -0,0 +1,9 @@
+// 새싹
+// https://www.acmicpc.net/problem/25083
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
