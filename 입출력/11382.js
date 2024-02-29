b1333aea1dbdd84810fc0338856981bbc1abac8a
spde3289 <spde3289@naver.com>
2024-02-29 20:30:24 +0900

    꼬마 정민

--- 입출력/11382.js
+++ 입출력/11382.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
+// 꼬마 정민
+// https://www.acmicpc.net/problem/11382
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: fs.createReadStream("./ex.txt"),
+  input: process.stdin,
   output: process.stdout,
-  terminal: false,
 });
 
-let input ;
+let input;
 
 rl.on("line", (line) => {
-  input = line.split(" ").map(el => +el)
+  input = line.split(" ").map((el) => +el);
 }).on("close", () => {
   console.log(input[0] + input[1] + input[2]);
 });
